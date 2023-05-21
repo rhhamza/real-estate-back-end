603f5b200667cdd1791baa931332eae003326a3c
rahalihamza <hamza.rahali@dis4u-consulting.com>
2023-05-21 22:13:28 +0100

    Real Estate Back End : SprintBoot

---
+++ src/main/java/pi/dev/realestate/RealEstateBackEndApplication.java
@@ -0,0 +1,13 @@
+package pi.dev.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pi/dev/realestate/RealEstateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.dev.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
