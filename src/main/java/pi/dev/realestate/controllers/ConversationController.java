2d26e9b954504a6422e9e8b251ed721caa5e2639
rahalihamza <hamza.rahali@dis4u-consulting.com>
2023-07-04 15:38:23 +0100

    Fix confict

--- src/main/java/pi/dev/realestate/controllers/ConversationController.java
+++ src/main/java/pi/dev/realestate/controllers/ConversationController.java
@@ -1,86 +1,86 @@
-package pi.dev.realestate.controllers;
+// package pi.dev.realestate.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import pi.dev.realestate.entities.Conversation;
-import pi.dev.realestate.entities.Message;
-import pi.dev.realestate.entities.UserEntity;
-import pi.dev.realestate.services.interfaces.IConversationService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import pi.dev.realestate.entities.Conversation;
+// import pi.dev.realestate.entities.Message;
+// import pi.dev.realestate.entities.UserEntity;
+// import pi.dev.realestate.services.interfaces.IConversationService;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-@RestController
-@RequestMapping("/conversations")
-public class ConversationController {
-    @Autowired
-    private IConversationService iconversationService;
+// @RestController
+// @RequestMapping("/conversations")
+// public class ConversationController {
+//     @Autowired
+//     private IConversationService iconversationService;
 
-    @PostMapping
-    public ResponseEntity<Conversation> createConversation(@RequestBody Conversation conversation) {
-        Conversation createdConversation = iconversationService.createConversation(conversation);
-        return ResponseEntity.ok(createdConversation);
-    }
+//     @PostMapping
+//     public ResponseEntity<Conversation> createConversation(@RequestBody Conversation conversation) {
+//         Conversation createdConversation = iconversationService.createConversation(conversation);
+//         return ResponseEntity.ok(createdConversation);
+//     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<List<Conversation>> getConversationsByUser(@PathVariable int userId) {
-        List<Conversation> conversations = iconversationService.getConversationsByUser(userId);
-        return ResponseEntity.ok(conversations);
-    }
+//     @GetMapping("/{userId}")
+//     public ResponseEntity<List<Conversation>> getConversationsByUser(@PathVariable int userId) {
+//         List<Conversation> conversations = iconversationService.getConversationsByUser(userId);
+//         return ResponseEntity.ok(conversations);
+//     }
 
-    @PostMapping("/{conversationId}")
-    public ResponseEntity<Conversation> addParticipant(@PathVariable Long conversationId, @RequestBody UserEntity user) {
-        Conversation updatedConversation = iconversationService.addParticipant(conversationId, user);
-        return ResponseEntity.ok(updatedConversation);
-    }
+//     @PostMapping("/{conversationId}")
+//     public ResponseEntity<Conversation> addParticipant(@PathVariable Long conversationId, @RequestBody UserEntity user) {
+//         Conversation updatedConversation = iconversationService.addParticipant(conversationId, user);
+//         return ResponseEntity.ok(updatedConversation);
+//     }
 
-    @PutMapping("/{conversationId}")
-    public ResponseEntity<Conversation> updateConversation(
-            @PathVariable Long conversationId,
-            @RequestBody Message message) {
-        Conversation updatedConversation = iconversationService.updateConversation(conversationId, message);
-        return ResponseEntity.ok(updatedConversation);
-    }
+//     @PutMapping("/{conversationId}")
+//     public ResponseEntity<Conversation> updateConversation(
+//             @PathVariable Long conversationId,
+//             @RequestBody Message message) {
+//         Conversation updatedConversation = iconversationService.updateConversation(conversationId, message);
+//         return ResponseEntity.ok(updatedConversation);
+//     }
 
-    @GetMapping("/{conversationId}")
-    public Conversation getConversationByid(@PathVariable("conversationById") Long id){
-        return iconversationService.getConversationbyId(id);
-    }
+//     @GetMapping("/{conversationId}")
+//     public Conversation getConversationByid(@PathVariable("conversationById") Long id){
+//         return iconversationService.getConversationbyId(id);
+//     }
 
-    @GetMapping("/{firstName}/{lastName}")
-    public ResponseEntity<?> getConversationByParticipantName(
-            @PathVariable String firstName,
-            @PathVariable String lastName
-    ) {
-        try {
-            Optional<Conversation> conversation = iconversationService.getConversationByUserFirstNamAndLastName(firstName, lastName);
+//     @GetMapping("/{firstName}/{lastName}")
+//     public ResponseEntity<?> getConversationByParticipantName(
+//             @PathVariable String firstName,
+//             @PathVariable String lastName
+//     ) {
+//         try {
+//             Optional<Conversation> conversation = iconversationService.getConversationByUserFirstNamAndLastName(firstName, lastName);
 
-            if (conversation.isPresent()) {
-                return ResponseEntity.ok(conversation.get());
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving conversation");
-        }
-    }
-    @GetMapping("/search")
-    public ResponseEntity<?> getConversationByUserFirstNameOrLastName(
-            @RequestParam String name
-    ) {
-        try {
-            Optional<Conversation> conversation = iconversationService.getConversationByUserFirstNameOrLastName(name);
+//             if (conversation.isPresent()) {
+//                 return ResponseEntity.ok(conversation.get());
+//             } else {
+//                 return ResponseEntity.notFound().build();
+//             }
+//         } catch (Exception e) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving conversation");
+//         }
+//     }
+//     @GetMapping("/search")
+//     public ResponseEntity<?> getConversationByUserFirstNameOrLastName(
+//             @RequestParam String name
+//     ) {
+//         try {
+//             Optional<Conversation> conversation = iconversationService.getConversationByUserFirstNameOrLastName(name);
 
-            if (conversation.isPresent()) {
-                return ResponseEntity.ok(conversation.get());
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving conversation");
-        }
-    }
-}
+//             if (conversation.isPresent()) {
+//                 return ResponseEntity.ok(conversation.get());
+//             } else {
+//                 return ResponseEntity.notFound().build();
+//             }
+//         } catch (Exception e) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving conversation");
+//         }
+//     }
+// }
 
--- src/main/java/pi/dev/realestate/services/impl/ConversationService.java
+++ src/main/java/pi/dev/realestate/services/impl/ConversationService.java
@@ -1,73 +1,73 @@
-package pi.dev.realestate.services.impl;
+// package pi.dev.realestate.services.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import pi.dev.realestate.entities.Company;
-import pi.dev.realestate.entities.Conversation;
-import pi.dev.realestate.entities.Message;
-import pi.dev.realestate.entities.UserEntity;
-import pi.dev.realestate.exceptions.ConversationNotFoundException;
-import pi.dev.realestate.exceptions.ParticipantExistsException;
-import pi.dev.realestate.repositories.ConversationRepository;
-import pi.dev.realestate.services.interfaces.IConversationService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import pi.dev.realestate.entities.Company;
+// import pi.dev.realestate.entities.Conversation;
+// import pi.dev.realestate.entities.Message;
+// import pi.dev.realestate.entities.UserEntity;
+// import pi.dev.realestate.exceptions.ConversationNotFoundException;
+// import pi.dev.realestate.exceptions.ParticipantExistsException;
+// import pi.dev.realestate.repositories.ConversationRepository;
+// import pi.dev.realestate.services.interfaces.IConversationService;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-@Service
-public class ConversationService implements IConversationService {
-    @Autowired
-    private ConversationRepository conversationRepository;
+// @Service
+// public class ConversationService implements IConversationService {
+//     @Autowired
+//     private ConversationRepository conversationRepository;
 
-    @Override
-    public Conversation createConversation(Conversation conversation) {
-        return conversationRepository.save(conversation);
-    }
+//     @Override
+//     public Conversation createConversation(Conversation conversation) {
+//         return conversationRepository.save(conversation);
+//     }
 
-    @Override
-    public List<Conversation> getConversationsByUser(int company) {
-        return conversationRepository.findByParticipantsId(company);
-    }
+//     @Override
+//     public List<Conversation> getConversationsByUser(int company) {
+//         return conversationRepository.findByParticipantsId(company);
+//     }
 
-    @Override
-    public Optional<Conversation> getConversationByIdAndUser(Long conversationId, Company company) {
-        return conversationRepository.findByIdAndParticipants(conversationId, company);
-    }
-    @Override
-    public Conversation addParticipant(Long conversationId, UserEntity user) {
-        Optional<Conversation> optionalConversation = conversationRepository.findById(conversationId);
-        if (optionalConversation.isEmpty()) {
-            throw new ConversationNotFoundException("Conversation not found with ID: " + conversationId);
-        }
-        Conversation conversation = optionalConversation.get();
-        if (conversation.getParticipants().contains(user)) {
-            throw new ParticipantExistsException("User is already a participant in the conversation");
-        }
-        conversation.getParticipants().add(user);
-        Conversation updatedConversation = conversationRepository.save(conversation);
+//     @Override
+//     public Optional<Conversation> getConversationByIdAndUser(Long conversationId, Company company) {
+//         return conversationRepository.findByIdAndParticipants(conversationId, company);
+//     }
+//     @Override
+//     public Conversation addParticipant(Long conversationId, UserEntity user) {
+//         Optional<Conversation> optionalConversation = conversationRepository.findById(conversationId);
+//         if (optionalConversation.isEmpty()) {
+//             throw new ConversationNotFoundException("Conversation not found with ID: " + conversationId);
+//         }
+//         Conversation conversation = optionalConversation.get();
+//         if (conversation.getParticipants().contains(user)) {
+//             throw new ParticipantExistsException("User is already a participant in the conversation");
+//         }
+//         conversation.getParticipants().add(user);
+//         Conversation updatedConversation = conversationRepository.save(conversation);
 
-        return updatedConversation;
-    }
-    @Override
-    public Conversation updateConversation(Long conversationId, Message message) {
-        Optional<Conversation> conversationOptional = conversationRepository.findById(conversationId);
-        Conversation conversation = conversationOptional.get();
-        conversation.getMessages().add(message);
+//         return updatedConversation;
+//     }
+//     @Override
+//     public Conversation updateConversation(Long conversationId, Message message) {
+//         Optional<Conversation> conversationOptional = conversationRepository.findById(conversationId);
+//         Conversation conversation = conversationOptional.get();
+//         conversation.getMessages().add(message);
 
-        return conversationRepository.save(conversation);
-    }
+//         return conversationRepository.save(conversation);
+//     }
 
-    @Override
-    public Conversation getConversationbyId(Long id){
-        Optional<Conversation> optionalConversation = conversationRepository.findById(id);
-        return optionalConversation.get();
-    }
-    @Override
-    public Optional<Conversation> getConversationByUserFirstNamAndLastName(String firstName, String lastName) {
-        return conversationRepository.findByParticipantsFirstNameAndParticipantsLastName(firstName, lastName);
-    }
-    @Override
-    public Optional<Conversation> getConversationByUserFirstNameOrLastName(String name) {
-        return conversationRepository.findByParticipantsFirstNameOrParticipantsLastName(name);
-    }
-}
+//     @Override
+//     public Conversation getConversationbyId(Long id){
+//         Optional<Conversation> optionalConversation = conversationRepository.findById(id);
+//         return optionalConversation.get();
+//     }
+//     @Override
+//     public Optional<Conversation> getConversationByUserFirstNamAndLastName(String firstName, String lastName) {
+//         return conversationRepository.findByParticipantsFirstNameAndParticipantsLastName(firstName, lastName);
+//     }
+//     @Override
+//     public Optional<Conversation> getConversationByUserFirstNameOrLastName(String name) {
+//         return conversationRepository.findByParticipantsFirstNameOrParticipantsLastName(name);
+//     }
+// }
